215417ba4ca8dcda3db2751822a8a78646d307e5
Mary Yaschuk <feyaschuk@mail.ru>
2021-06-24 20:28:39 +0300

    fixed review1

--- homework.py
+++ homework.py
@@ -8,63 +8,75 @@ from telegram import Bot
 
 load_dotenv()
 
-PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+PRAKTIKUM_TOKEN = 'OAuth '+ os.environ['PRAKTIKUM_TOKEN']
+TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
+HEADERS = {"Authorization": PRAKTIKUM_TOKEN}
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+TIME_SLEEP = 5 * 60
+TIME_SLEEP_EXCEPTION = 5
 
 bot = Bot(token=TELEGRAM_TOKEN)
+
 logging.basicConfig(
     level=logging.DEBUG,
     filename='homework.log',
     filemode='a',
     format='%(asctime)s, %(levelname)s, %(message)s, %(name)s',)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-
-def parse_homework_status(homework):
-
-    homework_name = homework["homework_name"]
-    if homework["status"] == 'rejected':
+def parse_homework_status(homework):    
+    homework_name = homework.get("homework_name")
+    homework_status = homework.get("status")    
+    if homework_status == 'rejected':
         verdict = 'К сожалению, в работе нашлись ошибки.'
+    elif homework_status == 'reviewing':
+        verdict = 'Работа взята в ревью.'                              
     else:
-        verdict = 'Ревьюеру всё понравилось, работа зачтена!'
+        verdict = 'Ревьюеру всё понравилось, работа зачтена!'    
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
-
-def get_homeworks(current_timestamp):
-
-    headers = {
-        "Authorization": "OAuth AQAAAABNMV-PAAYckSNu9UA1D0RtpcM40Vl9NoA"}
+def get_homeworks(current_timestamp):   
     params = {'from_date': current_timestamp}
-    homework_statuses = requests.get(
-        'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
-        headers=headers, params=params)
+    homework_statuses = requests.get(URL, headers=HEADERS, params=params)
+    try:
+        homework_statuses.raise_for_status()        
+    except requests.exceptions.HTTPError as error:
+        print('Не удалось выполнить запрос. Ошибка: {error}')    
     return homework_statuses.json()
 
 
 def send_message(message):
-    logging.info
-    logging.debug
-    logging.error
-    return bot.send_message(chat_id=CHAT_ID, text=message)
+    try:        
+        return bot.send_message(chat_id=CHAT_ID, text=message)
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        print(f'Не удалось отправить сообщение, ошибка: {e}')            
 
-
-def main():
-    current_timestamp = int(time.time())
-    # current_timestamp = 1622604970  # Начальное значение timestamp
+def main():    
+    try:
+        current_timestamp = int(time.time())
+    except ValueError:
+            print("Дата должна быть в формате Unix")
+            time.sleep(TIME_SLEEP)           
     while True:
         try:
-            homeworks = get_homeworks(current_timestamp)
-            for homework in homeworks["homeworks"]:
-                if homework['status'] is not None:
-                    message = parse_homework_status(homework)
-                    send_message(message)
-            time.sleep(5 * 60)  # Опрашивать раз в пять минут
-
+            homeworks = get_homeworks((current_timestamp))
+            homework = homeworks.get("homeworks")[0]                        
+            message = parse_homework_status(homework)
+            send_message(message)
+            time.sleep(TIME_SLEEP)
+        
+        except IndexError:
+            print("Нет работ на проверке")
+            time.sleep(TIME_SLEEP)
         except Exception as e:
-            message = (f'{e}')
+            logging.error(e, exc_info=True) 
+            message = (f'Бот упал с ошибкой:{e}')
             send_message(message)
             print(f'Бот упал с ошибкой: {e}')
-            time.sleep(5)
+            time.sleep(TIME_SLEEP_EXCEPTION)
 
 
 if __name__ == '__main__':
