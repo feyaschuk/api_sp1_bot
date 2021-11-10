48e793e6adf7276c85ba1032353326b6a047a2b4
Mary Yaschuk <feyaschuk@mail.ru>
2021-11-10 16:30:23 +0300

    fix adress

--- homework.py
+++ homework.py
@@ -21,9 +21,9 @@ try:
     TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN'] 
     CHAT_ID = os.environ['TELEGRAM_CHAT_ID'] 
     HEADERS = {"Authorization": PRAKTIKUM_TOKEN} 
-    URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/' 
+    URL = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'     
     TIME_SLEEP = 20 * 60 
-    TIME_SLEEP_EXCEPTION = 5 
+    TIME_SLEEP_EXCEPTION = 60
 except Exception as e: 
     logging.error(e, exc_info=True) 
  
@@ -55,7 +55,7 @@ def parse_homework_status(homework):
 def get_homeworks(current_timestamp): 
     params = {'from_date': current_timestamp} 
     try: 
-        homework_statuses = requests.get(URL, headers=HEADERS, params=params) 
+        homework_statuses = requests.get(URL, headers=HEADERS, params=params)         
         if homework_statuses.status_code != 200: 
             homework_statuses.raise_for_status() 
         else: 
@@ -81,7 +81,7 @@ def main():
         time.sleep(TIME_SLEEP) 
     while True: 
         try: 
-            homeworks = get_homeworks((current_timestamp)) 
+            homeworks = get_homeworks((current_timestamp))             
             homework = homeworks.get("homeworks")[0] 
             message = parse_homework_status(homework) 
             send_message(message) 
